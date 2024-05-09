3fefd137e93da2990ddb772a9a9e63d3de9791a0
Bandish Basnet <bandishbasnet@gmail.com>
2024-05-09 22:49:58 +1000

    Vedanata school fiel added for live

---
+++ vedanta/assets/app.js
@@ -0,0 +1,11 @@
+const prevBtn = document.getElementById("prevbtn");
+const nextBtn = document.getElementById("nextbtn");
+const images = document.querySelectorAll("ul img");
+console.log(images);
+
+function previous(){
+    for(let i =0; i<images.length;i++){
+        document.getElementById("imagecontainer").style.display = "none;"
+        console
+    }
+}
