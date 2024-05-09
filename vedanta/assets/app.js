1462173cf20727dcf70342dbca7f58ce90fb8937
Bandish Basnet <bandishbasnet@gmail.com>
2024-05-10 00:48:59 +1000

    hamburger javascript added, Footer added

--- vedanta/assets/app.js
+++ vedanta/assets/app.js
@@ -1,11 +1,27 @@
-const prevBtn = document.getElementById("prevbtn");
-const nextBtn = document.getElementById("nextbtn");
-const images = document.querySelectorAll("ul img");
-console.log(images);
+function toggle(event) {
+    const targetButton = event.target;
+    const ansElementStyle = targetButton.parentElement.parentElement.querySelector('.faqans');
+    ansElementStyle.classList.toggle('show')
+    //     if(ansElementStyle.display !== 'none') {
+    //         ansElementStyle.display = 'none';
+    //     }
+    //     else {
+    //         ansElementStyle.display = 'block';
+    //     } 
+}
+const faqButtons = document.querySelectorAll('.faq button');
+faqButtons.forEach(button => {
+    button.addEventListener('click', toggle);
+})
 
-function previous(){
-    for(let i =0; i<images.length;i++){
-        document.getElementById("imagecontainer").style.display = "none;"
-        console
-    }
+// Mobile Menu Bar
+document.getElementById("hamburger").onclick = function(){
+    document.getElementById("mobilenavmenu").style.display = "block";
+    document.getElementById("closebtn").style.display = "block";
+    document.getElementById("hamburger").style.display = "none";
+}
+document.getElementById("closebtn").onclick = function(){
+    document.getElementById("mobilenavmenu").style.display = "none";
+    document.getElementById("closebtn").style.display = "none";
+    document.getElementById("hamburger").style.display = "block";
 }
